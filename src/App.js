c2fa844dbb8257a97563acd32793905e8f5f4cc2
Charlotte-code9 <medallacharlotte7@gmail.com>
2023-05-22 01:00:47 +0800

    Update homepage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <Router>
       <Header/>
         <Routes>
-            <Route path='/' element={ <Home/>}/>
+            <Route path='/pretty-gal-botique' element={ <Home/>}/>
             <Route path='/product/:id' element={ <ProductDetails/>}/>
             <Route path='/products' element={ <Products/>}/>
         </Routes>
